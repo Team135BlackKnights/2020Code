8ca102cbcc7c62ced7c6284ae8134c4e04bc3837
Keaton Smith <keatfish@gmail.com>
2020-02-15 10:40:40 -0500

    Update PathFollower.java

--- 2020Code/src/main/java/frc/robot/util/PathFollower.java
+++ 2020Code/src/main/java/frc/robot/util/PathFollower.java
@@ -7,21 +7,35 @@
 
 package frc.robot.util;
 
+import frc.robot.nsubsystems.FalconDrive;
+
 /**
  * Add your docs here.
  */
 public class PathFollower 
 {
-    public PathFollower(Waypoint[] waypoints)
-    {
+    public Segment[] segments;
+    public FalconDrive drive; 
+    public Waypoint[] waypoints;
 
+    public Segment currentSegment;
+
+    public PathFollower(Waypoint[] _waypoints, FalconDrive _drive)
+    {
+        drive = _drive;
+        waypoints = _waypoints;
     }
 
     public class Waypoint
     {
         public Waypoint(double x, double y, double theta, double speed)
         {
+            double waypointX, waypointY, waypointTheta, waypointsSpeed;
 
+            waypointX = x;
+            waypointY = y;
+            waypointTheta = theta;
+            waypointsSpeed = speed;
         }
     }
 
@@ -29,12 +43,29 @@ public class PathFollower
     {
         public Segment(Waypoint a, Waypoint b)
         {
+            Waypoint aCopy = a;
+            Waypoint bCopy = b; 
 
+            //boolean isSegmentLine = 0 <=(aCopy.waypointTheta - bCopy.waypointTheta);
         }
+
+        
     }
 
     public void segmentCreator(Waypoint[] waypoints)
     {
-        
+        int n = waypoints.length;
+        segments = new Segment[n-1];
+
+        for(int i = 0; i<n-1; i++)
+        {
+            segments[i] = new Segment(waypoints[i], waypoints[i+1]);
+        }
+    }
+
+
+    public void followPath()
+    {
+
     }
 }
