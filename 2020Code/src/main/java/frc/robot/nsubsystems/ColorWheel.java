a7c7b08feb8a467351d8ba785152bba4a1a2080f
Keaton Smith <robotics@phm.k12.in.us>
2020-03-05 16:17:59 -0500

    ColorWheel Organized and Commented 3.5.20

--- 2020Code/src/main/java/frc/robot/nsubsystems/ColorWheel.java
+++ 2020Code/src/main/java/frc/robot/nsubsystems/ColorWheel.java
@@ -8,7 +8,6 @@
 package frc.robot.nsubsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.RobotContainer;
 import frc.robot.RobotMap;
 import frc.robot.util.MotorControl;
 import edu.wpi.first.wpilibj.I2C;
@@ -57,120 +56,140 @@ public class ColorWheel extends SubsystemBase implements RobotMap.CONTROL_PANEL
       // Desired color is none
       desiredColor = "No Color";
 
+      // Prints the color wheel subsystem is active
       System.out.println("Color Wheel Initialized");
 
    }
 
    // Checks if the color it is seeing is blue by checking it against the min and
    // max tolerances of each color
-
-   public boolean IsBlue() {
+   public boolean IsBlue()
+   {
       return ((red() >= BlueRedMin && red() <= BlueRedMax) && (green() >= BlueGreenMin && green() <= BlueGreenMax)
             && (blue() >= BlueBlueMin && blue() <= BlueBlueMax));
    }
 
    // Checks if the color it is seeing is red by checking it against the min and
    // max tolerances of each color
-
-   public boolean IsRed() {
+   public boolean IsRed()
+   {
       return ((red() >= RedRedMin && red() <= RedRedMax) && (green() >= RedGreenMin && green() <= RedGreenMax)
             && (blue() >= RedBlueMin && blue() <= RedBlueMax));
    }
 
    // Checks if the color it is seeing is green by checking it against the min and
    // max tolerances of each color
-
-   public boolean IsGreen() {
+   public boolean IsGreen()
+   {
       return ((red() >= GreenRedMin && red() <= GreenRedMax) && (green() >= GreenGreenMin && green() <= GreenGreenMax)
             && (blue() >= GreenBlueMin && blue() <= GreenBlueMax));
    }
 
    // Checks if the color it is seeing is yellow by checking it against the min and
    // max tolerances of each color
-
-   public boolean IsYellow() {
+   public boolean IsYellow()
+   {
       return ((red() >= YellowRedMin && red() <= YellowRedMax)
             && (green() >= YellowGreenMin && green() <= YellowGreenMax)
             && (blue() >= YellowBlueMin && blue() <= YellowBlueMax));
    }
 
-   public void initCANSparkMax(CANSparkMax spark, IdleMode mode) {
+   public void initCANSparkMax(CANSparkMax spark, IdleMode mode)
+   {
       spark.setInverted(false);
       spark.enableVoltageCompensation(12);
       spark.setIdleMode(mode);
    }
 
-   public String gameColor() {
+   public String gameColor()
+   {
       // Checks if the gamedata has arrived yet by making sure its length is greater
       // than 0
-      if (gameData.length() > 0) {
+      if (gameData.length() > 0)
+      {
          // Takes the Game Specific Data and changes it to a usable form
-         switch (gameData.charAt(0)) {
+         switch (gameData.charAt(0))
+         {
          case 'B':
             //Color to reach is red
             desiredColor = "Red";
             break;
+
          case 'G':
             //Color to reach is yellow
             desiredColor = "Yellow";
             break;
+
          case 'R':
             //Color to reach is blue
             desiredColor = "Blue";
             break;
+
          case 'Y':
             //Color to reach is green
             desiredColor = "Green";
             break;
+
          default:
             //No color has been recieved to reach
             desiredColor = "No color";
             break;
          }
-      } else {
+      } 
+      
+      // Code for no data received yet
+      else
+      {
          desiredColor = "No Color";
-         // Code for no data received yet
       }
+
       return desiredColor;
    }
 
    
 
    // Detect current Red value given from the color sensor
-   public double red() {
+   public double red()
+   {
       return detectedColor.red;
    }
 
    // Detect current Green value given from the color sensor
-   public double green() {
+   public double green()
+   {
       return detectedColor.green;
    }
 
    // Detect current Blue value given from the color sensor
-   public double blue() {
+   public double blue()
+   {
       return detectedColor.blue;
    }
 
    // Determine what the current color under the color sensor is
-   public String checkForColor() {
-
+   public String checkForColor()
+   {
       //The current color is blue
-      if (IsBlue()) {
+      if (IsBlue())
+      {
          return "Blue";
       }
 
       //The current color is red
-      else if (IsRed()) {
+      else if (IsRed())
+      {
          return "Red";
       }
 
       //The current color is green
-      else if (IsGreen()) {
+      else if (IsGreen())
+      {
          return "Green";
       }
 
       //The current color is yellow
-      else if (IsYellow()) {
+      else if (IsYellow())
+      {
          return "Yellow";
       }
 
@@ -179,8 +198,8 @@ public class ColorWheel extends SubsystemBase implements RobotMap.CONTROL_PANEL
          return "No Color";
    }
 
-   public void countColor() {
-
+   public void countColor()
+   {
       // Prints the last seen color to dash
       SmartDashboard.putString("Last Seen Color:", lastSeenColor);
 
@@ -204,49 +223,63 @@ public class ColorWheel extends SubsystemBase implements RobotMap.CONTROL_PANEL
       rotatorSpark.set(0);
    }
 
+   // Using subsystem to move control panel
    public void moveColorWheel(double power)
    {
       power = MotorControl.limit(power, .85, -.85);
       rotatorSpark.set(power);
    }
 
-   public void printOut() {
-      // Sets detected color to the current color seen by the color sensor(An RGB
-      // value)
+   // Gathers, sets, and displays information from the color sensor
+   public void printOut()
+   {
+      // Sets detected color to the current color seen by the color sensor
+      // (An RGB value)
       detectedColor = controlPanelColorSensor.getColor();
+
       // Sets current color to the color currently seen by the sensor(In string form)
       currentColor = checkForColor();
+
       // Runs the function to count colors
       countColor();
 
-      // Prints the different color values, the current color, the number of color
-      // changes, and the desired color to smart dash
+      // Prints the different color values to smart dash
       SmartDashboard.putNumber("color red", red());
       SmartDashboard.putNumber("color green", green());
       SmartDashboard.putNumber("color blue  ", blue());
 
+      // Prints the current color to smart dash
       SmartDashboard.putString("Current Color", currentColor);
+
+      // Prints the amount of color changes to smart dash
       SmartDashboard.putNumber("ColorChanges:", colorChanges);
 
+      // Prints the desired color to smart dash
       SmartDashboard.putString("Desired Color ", desiredColor);
    }
 
    // Pulls the game specific message from driver station
-
    @Override
-   public void periodic() {
+   public void periodic()
+   {
       detectedColor = controlPanelColorSensor.getColor();
+
+      // The desired color for a phase of the match
       gameColor();
+
+      // Prints the desired color at any given time to smart dash
       SmartDashboard.putString("Desired Color", gameColor());
+
       // Sets the current color to the current color(String)
       currentColor = checkForColor();
+
       // Counts color changes
       countColor();
 
-      // Prints to the screen
+      // Prints to the smart dash
       SmartDashboard.putString("Current Color", currentColor);
       SmartDashboard.putNumber("ColorChanges:", colorChanges);
-      // This method will be called once per scheduler run
+      // This method will be called once per scheduled run
    }
 
 }
